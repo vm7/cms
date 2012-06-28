8d4ce45522378a533cb6f4099c74431a5ac42d7a
vm <vedranmoric@yahoo.com>
2012-06-28 10:01:30 +0200

    initial commit

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.3'
+gem 'jquery-rails'
+gem 'gravatar_image_tag'
+gem 'will_paginate'
+gem 'pg'
+gem 'execjs'
+gem 'therubyracer'
+gem 'json'
+
+gem 'sqlite3'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+
+group :development do
+  gem 'annotate', '~> 2.4.1.beta'
+end
+
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cms::Application.config.secret_token = 'd6034b721776316eb0411f527668b7d981f53590090fb54935edf5c7100248fe76f181725a4b029a92380a40f1eec10c1b8519312ab8ae99f6f7dbcf0b335724'
